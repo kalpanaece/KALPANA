db031c115967a1f50027c40339a04ef4e4309cdc
kalpanaece <35127951+kalpanaece@users.noreply.github.com>
2018-01-10 16:16:30 +0530

    Create char.c

---
+++ char.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int ch;
+printf("enter the charactor");
+scanf("%d",&ch);
+if(ch=='a'//ch=='e'//ch=='i'//ch=='o'//ch=='u')
+{
+printf("enter the charactor is vowel");
+}
+else
+{
+printf("enter the charactor is constant");
+}
+return 0;
+}
