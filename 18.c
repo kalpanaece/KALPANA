2685ee296844bec6906b5ebb78cad5a2e083e2fc
kalpanaece <35127951+kalpanaece@users.noreply.github.com>
2018-01-21 05:43:40 +0000

    Create 18.c

---
+++ 18.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int a,b,i,temp,num;
+printf("enter two numbers");
+scanf("%d %d",&a,&b);
+printf("amstrong number betweem %d an %d");
+for(i=a+1;i<b;i++)
+{
+temp=i;
+num=0;
+while(temp!=0)
+{
+rem=(temp%10);
+num=rem*rem*rem;
+temp/=10;
+}
+if(i==num)
+{
+printf("%d",i);
+}
+}
