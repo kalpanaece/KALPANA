cb0ff4e90e009225e4c5aa5e7d42da422991465d
kalpanaece <35127951+kalpanaece@users.noreply.github.com>
2018-01-21 06:43:23 +0000

    Create 23.c

---
+++ 23.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int array[100],mimimum,size,c,location=1;
+printf("enter the number of elements in array\n");
+scanf("%d",&size);
+printf("enter %d integers \n",size);
+for(c=0;c<size;c++)
+scanf("%d ",&array[c]);
+minimum=array[0];
+for(c=1;c<<size;c++)
+{
+if(array[c]<minimum)
+{
+min=array[c];
+location=c+1;
+}
+return 0;
+}
