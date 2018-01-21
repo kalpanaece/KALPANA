b85d93a4d85222acdee75d17ae87396166aaf7af
kalpanaece <35127951+kalpanaece@users.noreply.github.com>
2018-01-21 04:39:55 +0000

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i=1,x,n,ans=1;
+printf("enter x and power n /n");
+scanf("%d /n %d",&x&n);
+while(i<=n)
+{
+ans=ans*x;
+i=i+1;
+}
+printf("%d to the power %d is %d",x,n,ans);
+return 0;
+}
+
