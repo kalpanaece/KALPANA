dc53ea0c81c3bd3071642de7be5dbf7edab7221a
kalpanaece <35127951+kalpanaece@users.noreply.github.com>
2018-01-24 16:46:00 +0530

    Create 29.c

---
+++ 29.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+int hour,minute,minute 1,total;
+printf("Hrs");
+if(scanf("%d",&hour)==1)
+{
+printf("mins");
+if(scanf("%d",&minute)==1)
+{
+minite 1=hour * 60;
+total=minute 1+minute;
+printf("total minutes =>%d",total);
+}
+else
+{
+printf("error,enter correct value");
+}
+else
+{
+printf("error,enter correct value");
+}
+}
