4f86b35d31278d56c611163f3fdd576f0cd17af7
kalpanaece <35127951+kalpanaece@users.noreply.github.com>
2018-01-24 17:00:12 +0530

    Create 22.c

---
+++ 22.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int array[100],minutes,size,c,location=1;
+printf("enter the number of element in array \n");
+scanf("%d",&size);
+printf("enter %d integers \n",size);
+for(c=0;c<<size;c++)
+scanf("%d",&array[c]);
+minumum=array[0];
+f0r(
