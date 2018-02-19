ecab1580ae5dc8fa0af224f3e31382445a3add92
kalpanaece <35127951+kalpanaece@users.noreply.github.com>
2018-02-19 15:42:46 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,r;
+printf("enter the number");
+scanf("%d",&r);
+n=[1,2,3,4,5,6,7,8,9,0];
+if(n=r)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
