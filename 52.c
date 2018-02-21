87329805497e36c7bf624e4d20e6fa078ba8c897
kalpanaece <35127951+kalpanaece@users.noreply.github.com>
2018-02-21 16:59:04 +0530

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int i,num=0;
+    printf("enter the i value");
+    scanf("%d",&i);
+    while(num!=0)
+    {
+        num=i+10;
+        printf("%d",num);
+        }
+switch(i)
+{
+    case 1:
+    printf("one");
+    break;
+    case 2:
+    printf("two");
+    break;
+    case 3:
+    printf("three");
+    break;
+    return 0;
+}
+}
+
+   
