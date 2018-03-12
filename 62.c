7075994a3e1eb159a6b7eceff5dd0bee23c228e8
kalpanaece <35127951+kalpanaece@users.noreply.github.com>
2018-03-12 16:34:47 +0530

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a/2==50)
+{
+printf("the number is binary");
+}
+else
+{
+	printf("the number is not binary");
+}
+
+}
