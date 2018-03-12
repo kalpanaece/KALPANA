c66eb43852b41d388748719ce3665acafb00c563
kalpanaece <35127951+kalpanaece@users.noreply.github.com>
2018-03-12 16:53:35 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the number");
+	scanf("%d",&a);
+	if(a/2=100)
+	{
+		printf("given number is prime");
+	}
+	else
+	{
+		printf("given number is not prime");
+	}
+	return 0;
+}
